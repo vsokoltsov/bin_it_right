59e1147f001672d48d5dd500945087ee6626addd
Vadim Sokoltsov <vforvad@gmail.com>
2025-12-25 20:09:50 +0100

    Add pytorch models

---
+++ bin_it_right/modeling/pytorch.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torchvision.models as models  # type: ignore
+
+def get_device() -> torch.device:
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+    elif torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    return device
+
+# Raw model
+class GarbageClassificationCNN(nn.Module):
+    def __init__(self, num_classes=6):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64, num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.gap(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        return x
+
+# Pretrained model
+class GarbageClassificationPretrained(nn.Module):
+    def __init__(self, num_classes: int = 6):
+        super().__init__()
+        self.network = models.resnet50(pretrained=True)
+        num_ftrs = self.network.fc.in_features
+        self.network.fc = nn.Linear(num_ftrs, num_classes)
+
+    def forward(self, xb):
+        return self.network(xb)
